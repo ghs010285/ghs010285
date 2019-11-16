076ba746f60210e7adc3f43d2dc53618f2f20562
ghs010285 <ghs010285@noreply.github.com>
2019-11-16 16:18:47 +0900

    Revert "안드로이드 프로젝트"
    This reverts commit 516721760f1851b95d613270d71e6871f0fbebee.

--- VenoApp/app/src/main/java/com/veno_clan/firebaseapp/venocommunity/model/UserModel.kt
+++
@@ -1,9 +0,0 @@
-package com.veno_clan.firebaseapp.venocommunity.model
-
-class UserModel {
-    var userName: String? = null
-    var userEmail: String? = null
-    var profileImageUrl: String? = null
-    var uid: String? = null
-    var pushToken: String? = null
-}
